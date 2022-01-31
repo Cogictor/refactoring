71306fb8bd0f5d15cf2e7e31526ff6add410e65f
Daniel Victor <vicdaniel.ferreira@gmail.com>
2022-01-31 20:17:31 -0300

    Passo 6: Extract and Move Method
    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,8 +41,5 @@ public class Movie {
   }
 
   public int getFrequentRenterPoints(int daysRented) { 
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-            daysRented > 1) return 2;
-      return 1;
-  }
+      return _price.getFrequentRenterPoints(daysRented);
 }
