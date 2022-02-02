5c0c6505cad5c231a746e13ed3f9245798c372f7
Daniel Victor <vicdaniel.ferreira@gmail.com>
2022-02-02 12:18:19 -0300

    Template Method - Parte 2
    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,5 +1,3 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
 
    public String getStartResult (Customer aCustomer){
@@ -20,16 +18,4 @@ public class HtmlStatement extends Statement {
       "</EM> frequent renter points<P>";
    }
 
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = getStartResult(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += getMiddleResult(each);
-      }
-      //add footer lines
-      result += getEndResult(aCustomer);
-      return result;
-   }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,5 +1,3 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
 
    public String getStartResult (Customer aCustomer){
@@ -20,16 +18,4 @@ public class TextStatement extends Statement {
       " frequent renter points";
    }
 
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = getStartResult(aCustomer);
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += getMiddleResult(each);
-      }
-      //add footer lines
-      result += getEndResult(aCustomer);
-      return result;
-   }
 }
